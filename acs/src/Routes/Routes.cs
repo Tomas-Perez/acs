24ef1afe18d1d1bfc08eeb6b003f929f034e2d4c
Dwape <eduardo.lalor@ing.austral.edu.ar>
2019-06-13 22:13:52 -0300

    Started implementing some routes.

--- acs/src/Routes/Routes.cs
+++ acs/src/Routes/Routes.cs
@@ -1,17 +1,42 @@
 using System;
+using acs.Db;
+using acs.Model;
+using acs.Repository;
+using acs.Service;
 using Nancy;
+using Nancy.Extensions;
 using Nancy.Hosting.Self;
+using Nancy.Json;
+using Nancy.ModelBinding;
 
 namespace acs.Routes
 {
     public sealed class Routes: NancyModule
     {
+        private UserService userService;
         
-        public Routes()
-        {
+        public Routes(UserService userService)
+        {   
+            this.userService = userService;
+            
             Get("/check",
                 _ => HttpStatusCode.OK);
             
+            /*
+            Get("/users/{id}",
+                _ => );
+                */
+            
+            Post("/users",
+                _ =>
+                {
+                    // Console.Write(Request.Body);
+                    UserForm userForm = this.Bind();
+                    userService.Register(userForm);
+                    JavaScriptSerializer serializer = new JavaScriptSerializer();
+                    var json = serializer.Serialize(userForm);
+                    return HttpStatusCode.OK;
+                });
         }
     }
 
